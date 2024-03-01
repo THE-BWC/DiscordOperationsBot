ea1be51123e4d3661c202320bacbfe45bb8411a3
Goltred <goltred@hotmail.com>
2024-03-01 00:01:33 -0500

    Added User model to retrieve leader and logic to send 30 minutes notification

--- bot.py
+++ bot.py
@@ -1,6 +1,8 @@
+import datetime
 import random
 import platform
 import os
+from typing import Callable, List
 
 import settings
 import botLogger
@@ -47,8 +49,61 @@ class DiscordBot(discord.Client):
         self.status_task.start()
         self.database = database
         await self.create_operation_embed()
+        await self.send_30minutes_notification_task()
 
-    async def create_operation_embed(self):
+    async def send_upcoming_ops(self):
+        await self.send_operations("OPSEC Operations", self.where_upcoming_opsec_ops)
+
+    @staticmethod
+    def where_upcoming_opsec_ops(operation_model: database.Operation, game: int):
+        return [
+            operation_model.game_id == game,
+            operation_model.is_opsec == True,
+            operation_model.is_completed == False
+        ]
+
+    async def send_30minutes_notification_task(self):
+        await self.send_operations("Operations starting in 30 minutes!", self.where_ops_30minutes, False)
+
+    @staticmethod
+    def where_ops_30minutes(operation_model: database.Operation, game: int):
+        """Set of conditions that select records from the Operation model with a specific deadline"""
+        # We want to remove seconds and microseconds from the equation for the query to be more flexible
+        deadline = datetime.datetime.now() + datetime.timedelta(minutes=30)
+        deadline = deadline.replace(second=0, microsecond=0)
+
+        # Mindful with boolean conditions here. We cannot use proper "pythonic" conditions like
+        # `operation_model.is_complete is False` because it doesn't translate properly in the SQL query
+        return [operation_model.game_id == game,
+                operation_model.is_completed == False,
+                operation_model.date_start.truncate("minute") == deadline]
+
+    def create_operations_embed(self, title: str, operations: [database.Operation]):
+        """Create the embed to be used by operation messages"""
+        embed = discord.Embed(
+            title=title,
+            color=discord.Color.red(),
+            type="rich"
+        )
+
+        for operation in operations:
+            opserv_link = "https://www.the-bwc.com"
+            field_title = operation.operation_name
+            lines = [
+                f"**Leader:** {operation.leader_user_id.name}",
+                f"**Start:** {operation.date_start}",
+                f"_Go to [Opserv]({opserv_link}) for details_"
+            ]
+
+            embed.add_field(
+                name=field_title,
+                value='\n'.join(lines),
+                inline=False
+            )
+
+        return embed
+
+    async def send_operations(self, embed_title:str, conditions: Callable[[database.Operation, int], List[bool]], include_timestamp = True):
         channels = self.config.OPSEC_CHANNELS_MAP
         for game, channel in channels.items():
             text_channel = await self.fetch_channel(channel)
@@ -57,27 +112,19 @@ class DiscordBot(discord.Client):
                 self.logger.error(f"Channel {text_channel} not found.")
                 return
 
-            operation_model = self.database.Operation
-            operations = operation_model.select().where(
-                operation_model.game_id == game,
-                operation_model.is_opsec == True,
-                operation_model.is_completed == False
-            )
+            operation_model = database.Operation
+            operations = operation_model.select().where(*conditions(operation_model, game))
 
-            embed = discord.Embed(
-                title="OPSEC Operations",
-                color=discord.Color.red()
-            )
-            for operation in operations:
-                embed.add_field(
-                    name=operation.operation_name,
-                    value=f"Start: <t:{operation.date_start}>\nEnd: <t:{operation.date_end}>",
-                    inline=False
-                )
-            # Get current timestamp
-            timestamp = discord.utils.utcnow()
-            embed.set_footer(text=f"Last updated: {timestamp}")
-            await text_channel.send(embed=embed)
+            if len(operations) == 0:
+                return
+
+            embed = self.create_operations_embed(embed_title, operations)
+
+            if include_timestamp:
+                # Get current timestamp
+                timestamp = discord.utils.utcnow()
+                embed.set_footer(text=f"Last updated: {timestamp}")
+                await text_channel.send(embed=embed)
 
 
 bot = DiscordBot()
--- database.py
+++ database.py
@@ -1,5 +1,5 @@
 import settings
-from peewee import Model, MySQLDatabase, IntegerField, CharField, BooleanField, DateTimeField
+from peewee import Model, MySQLDatabase, IntegerField, CharField, BooleanField, DateTimeField, ForeignKeyField
 
 
 xenforo = MySQLDatabase(
@@ -10,6 +10,14 @@ xenforo = MySQLDatabase(
     port=int(settings.XENFORO_DB_PORT)
 )
 
+# TODO: need to set the correct table_name override and complete with other relevant fields
+class User(Model):
+    id = IntegerField(primary_key=True)
+    name = CharField()
+
+    class Meta:
+        database = xenforo
+
 
 class Operation(Model):
     operation_id = IntegerField(primary_key=True)
@@ -18,7 +26,7 @@ class Operation(Model):
     type_id = IntegerField()
     date_start = DateTimeField()
     date_end = DateTimeField()
-    leader_user_id = IntegerField()
+    leader_user_id = ForeignKeyField(User)
     game_id = IntegerField()
     is_opsec = BooleanField()
 
